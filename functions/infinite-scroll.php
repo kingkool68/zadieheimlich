7e24076a0f269614b15a33d6db6faa7c55d41263
Russell Heimlich <kingkool68@gmail.com>
2016-02-14 00:05:35 -0500

    Documented the various infinite scroll functions.

--- functions/infinite-scroll.php
+++ functions/infinite-scroll.php
@@ -1,16 +1,15 @@
 <?php
-
-// Lets make rewrites work properly
+// Make /pages/ rewrite rules work
 function zah_infinite_scroll_rewrite_rules() {
 	add_rewrite_endpoint( 'pages', EP_ALL );
 }
 add_action('init', 'zah_infinite_scroll_rewrite_rules');
 
+//
 function zah_infinite_scroll_pre_get_posts( $query ) {
     if ( is_admin() || !$query->is_main_query() ) {
         return;
 	}
-
 
 	if ( $query->is_archive() && ( $pages = zah_infinite_scroll_get_pages() ) ) {
 		$start = $pages->start;
@@ -26,6 +25,7 @@ function zah_infinite_scroll_pre_get_posts( $query ) {
 }
 add_action( 'pre_get_posts', 'zah_infinite_scroll_pre_get_posts' );
 
+// Adjust the limit clause of the main SQL query
 function zah_infinite_scroll_post_limits( $limit ) {
 	global $wp_query;
 	if ( is_admin() || !$wp_query->is_main_query() ) {
@@ -38,7 +38,7 @@ function zah_infinite_scroll_post_limits( $limit ) {
 		}
 
 		$new_limit = $pages->diff * $pages->posts_per_page + $pages->posts_per_page;
-		$limit = str_replace(', ' . $pages->posts_per_page, ', ' . $new_limit, $limit);
+		$limit = str_replace( ', ' . $pages->posts_per_page, ', ' . $new_limit, $limit );
 	}
 	return $limit;
 }
@@ -51,7 +51,7 @@ function zah_infinite_scroll_wp() {
 		return;
 	}
 
-	$wp_query->set('paged', $pages->end);
+	$wp_query->set( 'paged', $pages->end );
 	$paged = get_query_var( 'paged' );
 }
 add_action( 'wp', 'zah_infinite_scroll_wp' );
@@ -59,20 +59,22 @@ add_action( 'wp', 'zah_infinite_scroll_wp' );
 function zah_infinite_scroll_redirect_canonical( $redirect_url, $requested_url ) {
 	// Kill the canonical 'paged' redirect if the pages query var is set. In other words we don't want /category/publications/?pages=1-10 to redirect to /category/publications/page/10/?pages=1-10 which is what would happen by default.
 	if( get_query_var('pages') ) {
-		$redirect_url = trailingslashit($requested_url);
+		$redirect_url = trailingslashit( $requested_url );
 	}
 	return $redirect_url;
 }
 add_filter( 'redirect_canonical', 'zah_infinite_scroll_redirect_canonical', 10, 2 );
 
-function zah_infinite_scroll_get_pagenum_link($result) {
+function zah_infinite_scroll_get_pagenum_link( $result ) {
 	if( $pages = zah_infinite_scroll_get_pages() ) {
-		$result = preg_replace('/pages\/([\d\-]+|all)\//i', '', $result); // Strip out anything that matches pages/{any digit or hypen or the word all}/
+		$result = preg_replace( '/pages\/([\d\-]+|all)\//i', '', $result ); // Strip out anything that matches pages/{any digit or hypen or the word all}/
 	}
+
 	return $result;
 }
-add_filter('get_pagenum_link', 'zah_infinite_scroll_get_pagenum_link');
+add_filter( 'get_pagenum_link', 'zah_infinite_scroll_get_pagenum_link' );
 
+// Determine if the request should load the infite scroll script or not
 function zah_infinite_scroll_enqueue_script() {
     wp_register_script( 'zah-infinite-scroll', get_template_directory_uri() . '/js/infinite-scroll.js', array('jquery'), NULL, true );
 
@@ -90,25 +92,34 @@ add_action( 'wp_enqueue_scripts', 'zah_infinite_scroll_enqueue_script' );
 * Helper Functions
 ***/
 
+// Get various details about the paging logic for the current request
 function zah_infinite_scroll_get_pages() {
+	global $wp;
 	$pages_var = get_query_var('pages');
 	if( !$pages_var ) {
 		return false;
 	}
+
+	// Inter request caching
+	if( property_exists( $wp, 'zah_infinite_scroll_page_details' ) && !empty( $wp->zah_infinite_scroll_page_details ) ) {
+
+		return $wp->zah_infinite_scroll_page_details;
+	}
+
 	$start = 1;
 	$end = false;
 	$pages = explode( '-', $pages_var );
 
-	if( count($pages) < 2 && isset($pages[0]) && !empty( $pages[0] ) ) {
-		if( strtolower($pages[0]) == 'all' ) {
+	if( count($pages) < 2 && isset( $pages[0] ) && !empty( $pages[0] ) ) {
+		if( strtolower( $pages[0] ) == 'all' ) {
 			$start = -1;
 			$end = false;
 		} else {
-			$end = intval($pages[0]);
+			$end = intval( $pages[0] );
 		}
 	} else {
-		$pages = array_map('intval', $pages);
-		if( isset($pages[0]) && !empty( $pages[0] ) ) {
+		$pages = array_map( 'intval', $pages );
+		if( isset( $pages[0] ) && !empty( $pages[0] ) ) {
 			$start = $pages[0];
 		}
 
@@ -117,10 +128,13 @@ function zah_infinite_scroll_get_pages() {
 		}
 	}
 
-	return (object) array(
+	$output = (object) array(
 		'start' => $start,
 		'end' => $end,
 		'diff' => abs( $end - $start ),
-		'posts_per_page' => intval(get_option('posts_per_page'))
+		'posts_per_page' => intval( get_option('posts_per_page') ),
 	);
+	$wp->zah_infinite_scroll_page_details = $output;
+
+	return $output;
 }
--- js/infinite-scroll.js
+++ js/infinite-scroll.js
@@ -1,4 +1,7 @@
-// via http://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript-jquery
+/*
+Generate a unique-ish hash for a given string
+via http://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript-jquery
+*/
 String.prototype.hashCode = function() {
 	var hash = 0, i = 0, len = this.length, chr;
 	while ( i < len ) {
@@ -11,25 +14,35 @@ jQuery(document).ready(function($) {
 	// Cached elements
 	var $head = $('head');
 	var $triggerElement = $('#content article').eq(-4);
+	// If the $triggerElement collection is empty then bail...
 	if( $triggerElement.length < 1 ) {
 		return;
 	}
 	// Set-up some constants.
-	var scrollTimeout;
 	var scrollUsePushStateInstead = false; // Set to true to make the history stack of the browser include every point when posts were loaded. It's kind of annoying.
-	var scrollDelay = 200; // Milliseconds
 	var scrollLoading = false;
-	var triggerOffset = $(document).height() - $triggerElement.offset().top; // The point of this is to do one calculation up front instead of multiple calculations every time the infinite scroll is triggered.
+	var triggerOffset = $(document).height() - $triggerElement.offset().top; // The point of this is to do one calculation up front instead of multiple calculations every time the infinite scroll event is triggered.
+
+	// Keep track of which scripts and styles have been loaded
 	var loadedScripts = [];
 	var loadedStyles = [];
-
 
 
 	// Simple feature detection for History Management (borrowed from Modernizr)
 	function supportsHistory() {
 		return !!(window.history && history.pushState);
 	}
+	if( !supportsHistory() ) {
+		return;
+	}
 
+	/*
+	For a given <script> element determine if it has already been loaded into the page.
+	If it hasn't been loaded then inject it:
+		1) Script src's get AJAX'd in
+		2) <script> blocks get appended to the <head>
+	Scripts of type "application/ld+json" are ignored.
+	 */
 	function maybeInjectScript( value, isInit ) {
 		if( typeof isInit === 'undefined' ) {
 			isInit = false;
@@ -64,6 +77,10 @@ jQuery(document).ready(function($) {
 		}
 	}
 
+	/*
+	For a given <style> or <link> element determine if it has already been loaded into the page.
+	If it hasn't been loaded then append it to the <head>
+	 */
 	function maybeInjectStyle( value, isInit ) {
 		if( typeof isInit === 'undefined' ) {
 			isInit = false;
@@ -85,22 +102,9 @@ jQuery(document).ready(function($) {
 			}
 		}
 	}
-
-	if( supportsHistory() ) {
-		$('script').each(function( index, script ) {
-			maybeInjectScript( script, true );
-		});
-		$('style,link[type="text/css"]').each(function( index, style ) {
-			maybeInjectStyle( style, true );
-		});
-
-		$(window).on('scroll', function() {
-			toInfinityAndBeyond();
-		});
-	}
-
 	// The function that does all of the work.
 	function toInfinityAndBeyond() {
+		// If we're waiting for a page request to complete, bail until it succeeds or fails
 		if( scrollLoading ) {
 			return;
 		}
@@ -115,11 +119,12 @@ jQuery(document).ready(function($) {
 				type: 'GET',
 				url: nextURL,
 				beforeSend: function() {
-					// block potentially concurrent requests
+					// Block potentially concurrent requests
 					scrollLoading = true;
 				},
 				success: function(data) {
 					$data = $(data);
+					// We need to manually parse the HTML returned in order to extract <script>, <style>, and <link> elements
 					theNodes = $.parseHTML( data, document, true );
 					$.each( theNodes, function( i, el ) {
 						if( el.nodeName.toUpperCase() === 'SCRIPT' ) {
@@ -131,9 +136,11 @@ jQuery(document).ready(function($) {
 						if( el.nodeName.toUpperCase() === 'LINK' && el.type.toLowerCase() === 'text/css' ) {
 							maybeInjectStyle( el );
 						}
-
 					});
+
+					//  Take #content from the AJAX'd page and inject it into the current page
 					$('#content').append( $data.find('#content').html() );
+					// Update the #pagination button with the value from the AJAX'd page
 					$('#pagination').before( $data.find('#pagination').outerHTML() ).remove();
 
 					var newPageNum = nextURL.match(/\/page\/(\d+)\//)[1];
@@ -158,12 +165,12 @@ jQuery(document).ready(function($) {
 						// We're dealing with /pages/x-x/
 						replacement = '/pages/' + pageStart + '-' + newPageNum + '/';
 						if( !pageEnd ) {
-							// We're dealing /page/x/ or /pages/x/
+							// We're dealing with /page/x/ or /pages/x/
 							replacement = '/pages/' + newPageNum + '/';
+							// If we're starting from a page then we need to modify the 'pages' range
 							if( pageLabel == 'page' ) {
 								replacement = '/pages/' + pageStart + '-' + newPageNum + '/';
 							}
-
 						}
 
 						newPath = newPath.replace( blackMagic, replacement);
@@ -179,19 +186,32 @@ jQuery(document).ready(function($) {
 						window.history.replaceState(null, null, newPath);
 					}
 
-					// unblock more requests (reset loading status)
+					// Unblock more requests (reset loading status)
 					scrollLoading = false;
 
-					// instantiate media element player for new video and audio elements injected on the page
+					// Instantiate media element player for new video and audio elements injected on to the page
 					if( typeof $.fn.mediaelementplayer === 'function' ) {
 						$('video,audio').mediaelementplayer();
 					}
 
 				},
-				dataType: "html"
+				dataType: 'html'
 			});
 		}
 	}
+
+	// Initial analysis of the scripts and styles loaded on the page
+	$('script').each(function( index, script ) {
+		maybeInjectScript( script, true );
+	});
+	$('style,link[type="text/css"]').each(function( index, style ) {
+		maybeInjectStyle( style, true );
+	});
+
+	$(window).on('scroll', function() {
+		toInfinityAndBeyond();
+	});
+
 });
 
 // Get the HTML of the entire element in a selector instead of just child elements.
