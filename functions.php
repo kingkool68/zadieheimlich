3464c96ee0e7a476110654e5a3251ba992d23a0b
Russell Heimlich <kingkool68@gmail.com>
2016-03-09 00:37:58 -0500

    Move scripts enqueued with the wp_head action to the footer. This keeps the script order in tact while capturing performance gains of moving all scripts to the bottom of the page.

--- functions.php
+++ functions.php
@@ -25,6 +25,13 @@ function zah_wp_enqueue_scripts() {
 	wp_register_style( 'zadie-heimlich', get_stylesheet_directory_uri() . '/css/zadie-heimlich' . $suffix, array('zah-google-fonts'), NULL, 'all' );
 	wp_enqueue_style( 'zadie-heimlich' );
 
+	/*
+	if( !rh_is_dev() && !is_admin() ) {
+		wp_register_script( 'zah-global-scripts', get_template_directory_uri() . '/js/global.min.js', array(), NULL, true );
+		wp_enqueue_script( 'zah-global-scripts' );
+	}
+	*/
+
 	wp_register_script( 'post-gallery', get_template_directory_uri() . '/js/post-gallery.js', array('jquery'), NULL, true );
 
 	// The mediaelement styles are rolled in to the zadie-heimlich.css file via Gulp
@@ -33,6 +40,41 @@ function zah_wp_enqueue_scripts() {
 	}
 }
 add_action( 'wp_enqueue_scripts', 'zah_wp_enqueue_scripts' );
+
+function zah_dequeue_script() {
+	if( rh_is_dev() || is_admin() ) {
+		return;
+	}
+	// 'wp-mediaelement'
+	$dequeue_script_handles = array( 'jquery', 'wp-embed', 'zah-menu' );
+	foreach( $dequeue_script_handles as $handle ) {
+		wp_dequeue_script( $handle );
+	}
+
+	global $wp_scripts;
+    foreach( $wp_scripts->queue as $handle ) {
+		var_dump( $wp_scripts->registered[ $handle ] );
+		$deps = $wp_scripts->registered[ $handle ]->deps;
+		foreach( $deps as $index => $dep ) {
+			if( in_array( $dep, $dequeue_script_handles) ) {
+				unset( $deps[ $index ] );
+			}
+		}
+		$wp_scripts->registered[ $handle ]->deps = $deps;
+	}
+}
+// add_action( 'wp_print_scripts', 'zah_dequeue_script', 5 );
+// add_action( 'wp_print_footer_scripts', 'zah_dequeue_script', 5 );
+
+// Move any scripts enquued to the wp_head action to the wp_footer action for performance reasons. See http://www.kevinleary.net/move-javascript-bottom-wordpress/ 
+function zah_move_head_scripts_to_footer() {
+	remove_action( 'wp_head', 'wp_print_scripts' );
+	remove_action( 'wp_head', 'wp_print_head_scripts', 9 );
+
+	add_action( 'wp_footer', 'wp_print_scripts', 5 );
+	add_action( 'wp_footer', 'wp_print_head_scripts', 5 );
+}
+add_action( 'after_setup_theme', 'zah_move_head_scripts_to_footer' );
 
 // Disable printing the WP Emjoi styles injected into the <head>. They're bundled into our compiled stylesheet.
 remove_action( 'wp_print_styles', 'print_emoji_styles' );
