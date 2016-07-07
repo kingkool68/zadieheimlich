8c69cb5fbae8e5faf9d34d43335a3af9308f07f1
Russell Heimlich <kingkool68@gmail.com>
2016-07-07 06:50:36 -0400

    Add a webmanifest file because why not?
    See https://developers.google.com/web/updates/2014/11/Support-for-installable-web-apps-with-webapp-manifest-in-chrome-38-for-Android?hl=en

--- header.php
+++ header.php
@@ -14,21 +14,22 @@
 <?php } ?>
 
 <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>">
-<link rel="apple-touch-icon" sizes="57x57" href="<?php echo content_url();?>/themes/zadie-heimlich/favicons/apple-touch-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="<?php echo content_url();?>/themes/zadie-heimlich/favicons/apple-touch-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="<?php echo content_url();?>/themes/zadie-heimlich/favicons/apple-touch-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="<?php echo content_url();?>/themes/zadie-heimlich/favicons/apple-touch-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="<?php echo content_url();?>/themes/zadie-heimlich/favicons/apple-touch-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="<?php echo content_url();?>/themes/zadie-heimlich/favicons/apple-touch-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="<?php echo content_url();?>/themes/zadie-heimlich/favicons/apple-touch-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="<?php echo content_url();?>/themes/zadie-heimlich/favicons/apple-touch-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="<?php echo content_url();?>/themes/zadie-heimlich/favicons/apple-touch-icon-180x180.png">
-<link rel="icon" type="image/png" href="<?php echo content_url();?>/themes/zadie-heimlich/favicons/favicon-32x32.png" sizes="32x32">
-<link rel="icon" type="image/png" href="<?php echo content_url();?>/themes/zadie-heimlich/favicons/android-chrome-192x192.png" sizes="192x192">
-<link rel="icon" type="image/png" href="<?php echo content_url();?>/themes/zadie-heimlich/favicons/favicon-96x96.png" sizes="96x96">
-<link rel="icon" type="image/png" href="<?php echo content_url();?>/themes/zadie-heimlich/favicons/favicon-16x16.png" sizes="16x16">
-<link rel="manifest" href="<?php echo content_url();?>/themes/zadie-heimlich/favicons/android-chrome-manifest.json">
-<link rel="shortcut icon" href="<?php echo content_url();?>/themes/zadie-heimlich/favicons/favicon.ico">
+<link rel="manifest" type="application/manifest+json" href="<?php echo esc_url( get_template_directory_uri() ); ?>/zadieheimlich.com.webmanifest.json">
+<link rel="apple-touch-icon" sizes="57x57" href="<?php echo esc_url( get_template_directory_uri() ); ?>/favicons/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="<?php echo esc_url( get_template_directory_uri() ); ?>/favicons/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="<?php echo esc_url( get_template_directory_uri() ); ?>/favicons/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="<?php echo esc_url( get_template_directory_uri() ); ?>/favicons/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="<?php echo esc_url( get_template_directory_uri() ); ?>/favicons/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="<?php echo esc_url( get_template_directory_uri() ); ?>/favicons/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="<?php echo esc_url( get_template_directory_uri() ); ?>/favicons/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="<?php echo esc_url( get_template_directory_uri() ); ?>/favicons/apple-touch-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo esc_url( get_template_directory_uri() ); ?>/favicons/apple-touch-icon-180x180.png">
+<link rel="icon" type="image/png" href="<?php echo esc_url( get_template_directory_uri() ); ?>/favicons/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="<?php echo esc_url( get_template_directory_uri() ); ?>/favicons/android-chrome-192x192.png" sizes="192x192">
+<link rel="icon" type="image/png" href="<?php echo esc_url( get_template_directory_uri() ); ?>/favicons/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="<?php echo esc_url( get_template_directory_uri() ); ?>/favicons/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="<?php echo esc_url( get_template_directory_uri() ); ?>/favicons/android-chrome-manifest.json">
+<link rel="shortcut icon" href="<?php echo esc_url( get_template_directory_uri() ); ?>/favicons/favicon.ico">
 <meta name="msapplication-TileColor" content="#7b0e7f">
 <meta name="msapplication-TileImage" content="/themes/zadie-heimlich/favicons/mstile-144x144.png">
 <meta name="msapplication-config" content="/themes/zadie-heimlich/favicons/browserconfig.xml">
