477c2f34bafaad0bbb2375473b270ee772369c37
Russell Heimlich <kingkool68@gmail.com>
2017-01-09 01:16:01 -0500

    Auto fix white space.

--- content-instagram.php
+++ content-instagram.php
@@ -1,7 +1,7 @@
 <article class="instagram">
-	
+
 	<?php do_action( 'zah_content_header', $post ); ?>
-	
+
 	<div class="inner">
 		<h1 class="title">
 			<time datetime="<?php echo get_post_time( 'c', true ) ?>">
@@ -14,10 +14,10 @@
 		</h1>
 		<h2 class="time-stamp"><?php echo how_old_was_zadie(); ?></h2>
 		<?php the_content(); ?>
-	
+
 		<p class="via">(via <a href="<?php echo $post->guid; ?>">@<?php echo get_instagram_username(); ?></a>)</p>
 	</div>
-	
+
 	<?php do_action( 'zah_content_footer', $post ); ?>
-	
+
 </article>
