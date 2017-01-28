00d7d1d2e863a9be43d28b64c13378b1744168c1
Russell Heimlich <kingkool68@gmail.com>
2017-01-28 11:54:08 -0500

    Add debugging option for Google Analytics.
    Changed the value of the action to the URL of the link clicked.

--- js/analytics.js
+++ js/analytics.js
@@ -1,16 +1,26 @@
 jQuery(document).ready(function($) {
-	if( typeof(__gaTracker) === 'function' ) {
+	var isDebugging = false;
+	if ( $('body').is('.debug-ga') ) {
+		isDebugging = true;
+	}
+
+	if( typeof __gaTracker === 'function' ) {
 		ga = __gaTracker;
 	}
-	if( typeof(ga) === 'undefined' ){
+	if( typeof ga === 'undefined' && ! isDebugging ){
 		return;
 	}
 
 	$('body').on('click', 'a[data-ga-category]', function(e) {
 		var $this = $(this);
 		var eventCategory = $this.data('ga-category');
-		var eventAction = 'click';
+		var eventAction = e.currentTarget.href;
 		var eventLabel = $this.data('ga-label');
+		if ( isDebugging ) {
+			console.log( 'Event:', eventCategory, eventAction, eventLabel );
+			e.preventDefault();
+			return;
+		}
 		ga('send', 'event', eventCategory, eventAction, eventLabel, {transport: 'beacon'});
 	});
 });
